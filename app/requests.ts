2a028e863c51bd6073e7a0b5418d902fcd7022f9
neversettle2018 <39088152+neversettle2018@users.noreply.github.com>
2023-03-31 23:41:28 +0800

    Update requests.ts

--- app/requests.ts
+++ app/requests.ts
@@ -130,7 +130,7 @@ export async function requestChatStream(
       finish();
     } else if (res.status === 402) {
       console.error("AnauthorizedAndIP");
-      responseText = Locale.Error.UnauthorizedAndIP;
+      responseText = Locale.Error1.Unauthorized;
       finish();
     }else {
       console.error("Stream Error");
