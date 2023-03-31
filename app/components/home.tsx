1d805617eba81f8709511849be884ed0777c4f6d
neversettle2018 <39088152+neversettle2018@users.noreply.github.com>
2023-03-31 17:13:04 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -593,7 +593,7 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>ChatGPT</div>
           <div className={styles["sidebar-sub-title"]}>
             Build your own AI assistant.
           </div>
