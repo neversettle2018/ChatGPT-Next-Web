3f6f27f64810ac0adcc7ad58b6631484c213b90d
neversettle2018 <39088152+neversettle2018@users.noreply.github.com>
2023-04-01 16:33:25 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ACCESS_CODES } from "./app/api/access";
 import md5 from "spark-md5";
-import { setCookie } from 'nookies';
 
 
 export const config = {
