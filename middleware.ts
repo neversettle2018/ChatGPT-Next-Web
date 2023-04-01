93c1a2aff4b06a00a55d90b9b479a95f802d1e24
neversettle2018 <39088152+neversettle2018@users.noreply.github.com>
2023-04-01 10:24:54 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { ACCESS_CODES } from "./app/api/access";
 import md5 from "spark-md5";
 
-const ipRequests = new Map<string, number>();
+let ipVisits = {}; // to keep track of IP visits
 
 export const config = {
   matcher: ["/api/chat", "/api/chat-stream"],
@@ -12,28 +12,36 @@ export function middleware(req: NextRequest, res: NextResponse) {
   const accessCode = req.headers.get("access-code");
   const token = req.headers.get("token");
   const hashedCode = md5.hash(accessCode ?? "").trim();
-  const ip = req.headers.get("x-forwarded-for") ?? "";
+  const ip = req.headers.get("x-real-ip");
   console.log("[Auth] allowed hashed codes: ", [...ACCESS_CODES]);
   console.log("[Auth] got access code:", accessCode);
   console.log("[Auth] hashed access code:", hashedCode);
   console.log("[Auth] ip:", ip);
   
   if (!accessCode && !token) {
-    console.log("invoke ip check...");
-    const requests = ipRequests.get(ip) ?? 0;
-    ipRequests.set(ip, requests + 1);
-    console.log("requests times:",requests);
     
-    if (requests >= 10) {
-      return NextResponse.json(
-        {
-          error: "Too many requests",
-        },
-        {
-          status: 402,
-        }
-      );
-    }
+     console.log("invoke ip check...");
+     console.log("ip times:", ipVisits[ip] );
+    
+     // check number of visits by IP
+      if(ip in ipVisits && ipVisits[ip] >= 10) {
+        return NextResponse.json(
+          {
+            message: 'Sorry! You have reached the maximum limit of visits from this IP address',
+          },
+          {
+            status: 402,
+          }
+        );
+      } else {
+        ipVisits[ip] = (ipVisits[ip] || 0) + 1;
+      }
+    
+    
+    
+    
+    
+    
   } else if (ACCESS_CODES.size > 0 && !ACCESS_CODES.has(hashedCode) && !token) {
     console.log("invoke orig");
     return NextResponse.json(
