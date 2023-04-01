19aa1f8973d5240662cc2a243167e86aa086de50
neversettle2018 <39088152+neversettle2018@users.noreply.github.com>
2023-04-01 14:24:03 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { ACCESS_CODES } from "./app/api/access";
 import md5 from "spark-md5";
 
+// 引入 http 以及 url 模块。
+import http from "http";
+import url from "url";
 
 export const config = {
   matcher: ["/api/chat", "/api/chat-stream"],
@@ -22,7 +25,26 @@ export function middleware(req: NextRequest, res: NextResponse) {
    
    console.log("invoke ip check...");
    
-
+  // 通过 url.parse 解析 URL 中的查询参数和路径。
+  const urlParts = url.parse(req.url, true);
+  const path = urlParts.pathname ?? "/";
+  const cookies = req.cookies;
+  
+  // 检查 IP 是否访问超过了 10 次。
+  const ipCount = cookies[ip] ?? 0;
+  if (ipCount >= 10) {
+    return NextResponse.json(
+      {
+        message: "IP 访问次数超限。",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+  
+  // 更新 IP 访问次数并保存到 cookie 中。
+  res.cookie(ip, ipCount + 1);
    
    
 
