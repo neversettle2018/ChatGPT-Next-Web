9be72012240d43b5f96cde7da6408178c4546e2c
neversettle2018 <39088152+neversettle2018@users.noreply.github.com>
2023-04-01 14:12:17 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { ACCESS_CODES } from "./app/api/access";
 import md5 from "spark-md5";
 
+// 对于每个IP，访问次数的存储数据结构 {ip1: count1, ip2: count2, ...} 
+const ipAccessCount = new Map();
+// 对于每个IP，访问次数保存在cookies中的名称
+const ipAccessCountCookieName = "IP_ACCESS_COUNT";
+// 限制IP最大访问次数 
+const maxAccessCount = 10;
+
 
 export const config = {
   matcher: ["/api/chat", "/api/chat-stream"],
@@ -22,7 +29,22 @@ export function middleware(req: NextRequest, res: NextResponse) {
    
    console.log("invoke ip check...");
    
-    
+
+   let accessCount = ipAccessCount.get(ip) ?? 0;
+   if (req.cookies[ipAccessCountCookieName]) {
+     accessCount = parseInt(req.cookies[ipAccessCountCookieName]); 
+    ipAccessCount.set(ip, accessCount); 
+   }
+ 
+   if (accessCount >= maxAccessCount) { 
+     return NextResponse.json( {
+       error: "IP access count exceeded", }, { status: 403, } 
+     );
+   }
+
+   accessCount++; 
+   ipAccessCount.set(ip, accessCount); 
+   res.cookie(ipAccessCountCookieName, accessCount.toString());
    
    
 
