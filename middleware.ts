5747d9edc28e88ba45d96c935cf6d37d78553f0b
neversettle2018 <39088152+neversettle2018@users.noreply.github.com>
2023-04-01 18:09:56 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { ACCESS_CODES } from "./app/api/access";
 import md5 from "spark-md5";
 
-export type { LocaleType } from "./app/locales/cn";
+
 
 export const config = {
   matcher: ["/api/chat", "/api/chat-stream"],
@@ -21,32 +21,13 @@ export function middleware(req: NextRequest, res: NextResponse) {
   console.log("[Auth] hashed access code:", hashedCode);
   console.log("[Auth] ip:", ipAddress);
   
- // Get IP request count from cookies or initialize as 0
- let requestCount = Number(req.cookies.get("requestCount") ?? 0);
+
   
  if (!accessCode && !token) {
    
  console.log("invoke ip check...");
 
-  // Get the current visit count for this IP or initialize it to zero
-  let visitCount = Number(localStorage.getItem(ipAddress)) || 0;
 
-  console.log("[IP Check] initial visit count:", visitCount);
-
-  // Increment the visit count and check whether it exceeds the limit
-  visitCount++;
-  if (visitCount > IP_LIMIT) {
-    console.log('[IP Check] IP address has reached maximum access limit');
-    return NextResponse.json(
-      { message: 'Too many requests from this IP address. Please try again later.' },
-      { status: 401 },
-    );
-  }
-
-  // Save the updated visit count in localStorage
-  localStorage.setItem(ipAddress, visitCount.toString());
-
-  console.log("[IP Check] updated visit count:", visitCount);
    
 
 
