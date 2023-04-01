1b10c23b2ba60840bd2a21a66c4a655d7427db0f
neversettle2018 <39088152+neversettle2018@users.noreply.github.com>
2023-04-01 10:36:53 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { ACCESS_CODES } from "./app/api/access";
 import md5 from "spark-md5";
 
-let ipVisits = {}; // to keep track of IP visits
+const ipVisits: { [key: string]: number } = {}; // to keep track of IP visits
 
 export const config = {
   matcher: ["/api/chat", "/api/chat-stream"],
